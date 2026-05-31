3cdca585ff5a47431dd4d1007b9d2fbb303b7e6e
rajmohan <rajmohan5323@gmail.com>
2026-05-31 09:42:11 +0530

    Java Configuration with deprecated package updated

--- kafka-producer-example/src/main/java/com/raj/config/KafkaProducerConfig.java
+++ kafka-producer-example/src/main/java/com/raj/config/KafkaProducerConfig.java
@@ -1,8 +1,18 @@
 package com.raj.config;
 
+//import com.fasterxml.jackson.databind.JsonSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -12,4 +22,22 @@ public class KafkaProducerConfig {
         return new NewTopic("springboot-events-demo", 3, (short) 1);
     }
 
+    @Bean
+    public Map<String,Object> producerConfig(){
+        Map<String,Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String,Object> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String,Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
